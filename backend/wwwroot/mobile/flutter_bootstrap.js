5b8ffa7596e3d010c6b3b2b2e3c218ffc45f53a9
David <david.simic@shiftonezero.com>
2026-02-06 14:10:19 +0100

    Deploy: all projects

--- backend/wwwroot/mobile/flutter_bootstrap.js
+++ backend/wwwroot/mobile/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4270842954"
+    serviceWorkerVersion: "1220726331"
   }
 });
